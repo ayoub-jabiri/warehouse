53274f784081a15485884619770c9441c50ed86c
Ayoub Jabiri <ayoubjabiri.business@gmail.com>
2026-07-24 11:24:40 +0100

    Finished the dockerfile and the docker-compose file

--- app/_lib/db.ts
+++ app/_lib/db.ts
@@ -3,6 +3,12 @@ import mongoose from "mongoose";
 const DATABASE_URL: string = process.env.DATABASE_URL!;
 
 export async function dbConnect() {
+
+    console.log("###############");
+    console.log(DATABASE_URL);
+    console.log("###############");
+    
+
     try {
         await mongoose.connect(DATABASE_URL);
 
